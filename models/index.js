3be282ea4780384a0d104abcb97bc8f747033966
leeggco <leeggco@gmail.com>
2020-03-13 22:49:27 +0800

    # 更新

--- models/index.js
+++ models/index.js
@@ -7,12 +7,23 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
+console.log(config)
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  // sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+  sequelize = new Sequelize(config.database, config.username, config.password,  {
+    host: config.host,    //数据库地址,默认本机
+    port: '3306',
+    dialect: config.dialect,
+    pool: {   //连接池设置
+      max: 5, //最大连接数
+      min: 0, //最小连接数
+      idle: 10000
+    },
+  });
 }
 
 fs
